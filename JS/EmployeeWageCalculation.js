00c1f6a99597a0f145c25c48730faeb8fff0cde3
Vishwas <baghel.vishwas0@gmail.com>
2022-08-01 15:27:02 +0530

    [Vishwas]refactor: Checking Employee Presence For Full Time Or Part Time By Using Switch Case

--- JS/EmployeeWageCalculation.js
+++ JS/EmployeeWageCalculation.js
@@ -7,23 +7,26 @@ const IS_PARTTIME = 2;
 const EMPWAGE_PER_HR = 20;
 let empHrs = 0;
 empCheck = Math.floor(Math.random()*10)%3;
-if (IS_FULLTIME == empCheck)
+switch(empCheck)
 {
-    console.log("UC1- Employee Is Present Full Time")
-    empHrs = 8;
-}
-else if(IS_PARTTIME == empCheck)
-{
-    console.log("UC1- Employee Is Present Part Time")
-    empHrs = 4;
-}
-else
-{
-    console.log("UC1- Employee Is Not Present")
-    empHrs = 0;
+    case IS_FULLTIME:
+        console.log("UC1- Employee Is Present Full Time")
+        empHrs = 8;
+        break;
+    case IS_PARTTIME:
+        console.log("UC1- Employee Is Present Part Time")
+        empHrs = 4;
+        break;
+    default:
+        console.log("UC1- Employee Is Not Present")
+        empHrs = 0;
+        break;
+
+
 }
 
 // UC2- Checking For The Part Time Or Full Time Working Hours
 empWage = empHrs * EMPWAGE_PER_HR;
 console.log("UC2- Employee Wage : "+ empWage);
 
+ 
