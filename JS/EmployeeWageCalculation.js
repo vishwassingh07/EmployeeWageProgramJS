4b9e70b51845081a906034ce49ff6f004732ea2f
Vishwas <baghel.vishwas0@gmail.com>
2022-08-01 14:19:59 +0530

    [Vishwas]add: Checking Employee Is Present For Full Time Or Part Time

--- JS/EmployeeWageCalculation.js
+++ JS/EmployeeWageCalculation.js
@@ -2,13 +2,28 @@ console.log("Welcome To The Employee Wage Program With JS")
 
 //UC1- Checking For Employee Present Or Not
 
-const IS_PRESENT = 1;
-empCheck = Math.floor(Math.random()*10)%2;
-if (IS_PRESENT == empCheck)
+const IS_FULLTIME = 1;
+const IS_PARTTIME = 2;
+const EMPWAGE_PER_HR = 20;
+let empHrs = 0;
+empCheck = Math.floor(Math.random()*10)%3;
+if (IS_FULLTIME == empCheck)
 {
-    console.log("Employee Is Present")
+    console.log("UC1- Employee Is Present Full Time")
+    empHrs = 8;
+}
+else if(IS_PARTTIME == empCheck)
+{
+    console.log("UC1- Employee Is Present Part Time")
+    empHrs = 4;
 }
 else
 {
-    console.log("Employee Is Not Present")
+    console.log("UC1- Employee Is Not Present")
+    empHrs = 0;
 }
+
+// UC2- Checking For The Part Time Or Full Time Working Hours
+empWage = empHrs * EMPWAGE_PER_HR;
+console.log("UC2- Employee Wage : "+ empWage);
+
