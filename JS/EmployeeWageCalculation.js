2c08a232ac74903793422cf748035f55f9d25209
Vishwas <baghel.vishwas0@gmail.com>
2022-08-01 15:57:00 +0530

    [Vishwas]add: Using Function To Get Working Hours Of Employee

--- JS/EmployeeWageCalculation.js
+++ JS/EmployeeWageCalculation.js
@@ -1,32 +1,31 @@
 console.log("Welcome To The Employee Wage Program With JS")
 
-//UC1- Checking For Employee Present Or Not
+//UC3- Using Function To Get Working Hours 
 
 const IS_FULLTIME = 1;
 const IS_PARTTIME = 2;
 const EMPWAGE_PER_HR = 20;
 let empHrs = 0;
 empCheck = Math.floor(Math.random()*10)%3;
-switch(empCheck)
+function GetEmployeeHrs(empCheck)
 {
-    case IS_FULLTIME:
-        console.log("UC1- Employee Is Present Full Time")
-        empHrs = 8;
-        break;
-    case IS_PARTTIME:
-        console.log("UC1- Employee Is Present Part Time")
-        empHrs = 4;
-        break;
-    default:
-        console.log("UC1- Employee Is Not Present")
-        empHrs = 0;
-        break;
-
-
+    switch(empCheck)
+    {
+        case IS_FULLTIME:
+            console.log("UC1- Employee Is Present Full Time")
+            empHrs = 8;
+            return empHrs;
+        case IS_PARTTIME:
+            console.log("UC1- Employee Is Present Part Time")
+            empHrs = 4;
+            return empHrs;
+        default:
+            console.log("UC1- Employee Is Not Present")
+            empHrs = 0;
+            return empHrs;
+    }
 }
-
-// UC2- Checking For The Part Time Or Full Time Working Hours
-empWage = empHrs * EMPWAGE_PER_HR;
+empWage = EMPWAGE_PER_HR * GetEmployeeHrs(empCheck);
 console.log("UC2- Employee Wage : "+ empWage);
 
  
