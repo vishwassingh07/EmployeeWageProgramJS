caf5b2157730eeaabd5f067b71086e18aa66a4ad
Vishwas <baghel.vishwas0@gmail.com>
2022-08-01 13:05:08 +0530

    [Vishwas]add: Checking Whether Employee Is Present Or Not

--- JS/EmployeeWageCalculation.js
+++ JS/EmployeeWageCalculation.js
@@ -1,1 +1,14 @@
 console.log("Welcome To The Employee Wage Program With JS")
+
+//UC1- Checking For Employee Present Or Not
+
+const IS_PRESENT = 1;
+empCheck = Math.floor(Math.random()*10)%2;
+if (IS_PRESENT == empCheck)
+{
+    console.log("Employee Is Present")
+}
+else
+{
+    console.log("Employee Is Not Present")
+}
