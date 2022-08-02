97b5fd576be59a8424c26e803ed268837a58fee2
Vishwas <baghel.vishwas0@gmail.com>
2022-08-02 12:48:37 +0530

    [Vishwas]add: Calculating Employee Wage For Maximum 20 Days Or 160 Hours In A Month

--- JS/EmployeeWageCalculation.js
+++ JS/EmployeeWageCalculation.js
@@ -1,13 +1,17 @@
 console.log("Welcome To The Employee Wage Program With JS")
 
-//UC4- Calculating Wage For 20 Days In A Month
+//UC5- Calculating Wage For 20 Days Or 160 Hours In A Month
 
 const IS_FULLTIME = 1;
 const IS_PARTTIME = 2;
 const EMPWAGE_PER_HR = 20;
 const MAX_WORKING_DAYS = 20;
+const MAX_WORKING_HRS = 160;
 let empHrs = 0;
 let totalEmpWage = 0;
+let day = 0;
+let workingHrs = 0;
+
 
 function GetEmployeeHrs(empCheck)
 {
@@ -27,12 +31,15 @@ function GetEmployeeHrs(empCheck)
             return empHrs;
     }
 }
-for(let day=1; day<=MAX_WORKING_DAYS; day++)
+while(day<=MAX_WORKING_DAYS && workingHrs<=MAX_WORKING_HRS)
 {
 empCheck = Math.floor(Math.random()*10)%3;
 empWage = EMPWAGE_PER_HR * GetEmployeeHrs(empCheck);
 console.log("UC2- Employee Wage For day "+ day + " is : "+ empWage);
 totalEmpWage += empWage;
+day++;
+workingHrs += empHrs;
 }
 console.log("UC4- Total Employee Wage : " + totalEmpWage);
+console.log("Total Working Days : "+(day-1)+ " Total Working Hours : "+workingHrs)
  
